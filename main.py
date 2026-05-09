b3adb9a6e3590971f4a095fb320abc65da8677c9
Alexander Igorevich <vt9166273054@gmail.com>
2026-05-10 02:30:49 +0300

    Add import module package homework

---
+++ main.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+
+from application.db.people import get_employees
+from application.salary import calculate_salary
+
+
+if __name__ == '__main__':
+    print(datetime.now().date())
+    calculate_salary()
+    get_employees()
